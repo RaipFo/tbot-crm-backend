63966569e3403764a5f58591d5f380f951cddf12
RaipFo <65996012+RaipFo@users.noreply.github.com>
2023-03-09 20:54:38 +0300

    Рефакторинг

--- tbot-crm-node-service/src/main/java/org/ebanda/service/MainService.java
+++ tbot-crm-node-service/src/main/java/org/ebanda/service/MainService.java
@@ -1,16 +1,27 @@
 package org.ebanda.service;
 
 import lombok.extern.log4j.Log4j;
+import org.ebanda.service.enums.ServiceCommands;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+
+
+/**
+создал тогда-то
+ **/
 @Service
 @Log4j
 public class MainService {
     private final ProducerService producerService;
 
+    /**
+     @Param
+     **/
+    @Autowired
     public MainService(ProducerService producerService) {
         this.producerService = producerService;
     }
